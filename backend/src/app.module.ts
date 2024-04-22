92a7f296a73de38f2fef3795a63639653ec8e774
HamiltonLopez <hamilt727@gmail.com>
2024-04-22 14:27:50 -0500

    Se crea el codigo del CRUD y se verfica la creacion de tablas en la base de datos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule, ConfigService} from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotaModule } from './nota/nota.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -22,7 +23,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
       inject: [ConfigService],
-    })
+    }),
+    NotaModule
     
     
   ],
